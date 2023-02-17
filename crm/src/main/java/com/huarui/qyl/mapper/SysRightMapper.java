e021ca5e609e56574e138b00a134867a22e6932b
小学生 <xiaopy_0701@qq.com>
2023-02-17 20:19:37 +0800

    Initial commit

--- crm/src/main/java/com/huarui/qyl/mapper/SysRightMapper.java
+++ crm/src/main/java/com/huarui/qyl/mapper/SysRightMapper.java
@@ -2,6 +2,7 @@ package com.huarui.qyl.mapper;
 
 
 import com.huarui.entity.SysRight;
+import com.huarui.entity.guoxigonnbiao;
 import org.apache.ibatis.annotations.Param;
 //@Param定义一个名字用来传值给mybatis#{roleId}
 //为什么要用因为如果是基本类型传多个要定义多个名字
@@ -10,5 +11,7 @@ import java.util.List;
 
 public interface SysRightMapper {
     public List<SysRight> gonnp(@Param("roleId") Integer roleId, @Param("rightParentId") Integer rightParentId);//通过前端登录的角色来看他可用的功能
+    public List<guoxigonnbiao> guox(@Param("roleId") Integer roleId, @Param("rightParentId") Integer rightParentId);//@Param("roleId") Integer roleId, @Param("rightParentId") Integer rightParentId
+    public List<guoxigonnbiao> guox2(@Param("roleId") Integer roleId, @Param("Right_code") Integer Right_code);
 }
 
